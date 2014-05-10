73dcc663f750eea2853dacf5bbc4ed3bf1c6da06
Max Gonzih <gonzih@gmail.com>
2014-05-10 18:32:22 +0200

    Fix chrome issue

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
-var container = document.querySelector('#container');
-var msnry = new Masonry( container, {
-  // options
-  columnWidth: 200,
-  itemSelector: '.item'
-});
+$(window).load(function() {
+    $('#container').masonry({
+        columnWidth: 200,
+        itemSelector: '.item'
+    })
+})
