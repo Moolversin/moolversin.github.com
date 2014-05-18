c79735b3b6a50e2681cd5e0c72d85b2a1ae0da4e
Max Gonzih <gonzih@gmail.com>
2014-05-18 12:31:10 +0200

    Add photo

--- template/about.html.clj
+++ template/about.html.clj
@@ -8,13 +8,16 @@
 
 [:div#container
  [:div.row
-  [:div.small-6.small-offset-2.columns
+  [:div.small-4.small-offset-3.columns
    [:p "I'm a varied artist, working with traditional and digital media in
-        realistic and surrealistic manner, living in Amsterdam, the Netherlands."]]
-  [:div.small-4.columns
+        realistic and surrealistic manner, currently living in Amsterdam, the Netherlands."]]
+  [:div.small-5.columns
    [:ul.vcard.contacts
     [:li.fn "Lubov Soltan"]
     [:li.email [:a {:href "mailto:lubarazgildeeva@gmail.com"} "Mail me!"]]
     [:li [:a {:href "https://plus.google.com/116635824809317777470/posts" :target "_blank"} "Google+"]]
     [:li [:a {:href "http://moolver-sin.deviantart.com/" :target "_blank"} "Devian Art"]]
-    [:li [:a {:href "https://www.behance.net/liubousoltan" :target "_blank"} "Behance"]]]]]]
+    [:li [:a {:href "https://www.behance.net/liubousoltan" :target "_blank"} "Behance"]]]]]
+ [:div.row
+  [:div.small-6.columns.small-centered
+   [:img {:src "/img/me.jpg"}]]]]
