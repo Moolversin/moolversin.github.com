0dddad828990fa33c4e08c79b418e538709792ad
Max Gonzih <gonzih@gmail.com>
2014-05-20 18:36:24 +0200

    Copy paste went wrong.

--- template/layouts/index.clj
+++ template/layouts/index.clj
@@ -92,10 +92,10 @@
 (defn to-images-html [coll]
   (map image coll))
 
-(def ga-script  "function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(def ga-script  "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
                  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                 (window,document,'script','//www.google-analytics.com/analytics.js','ga');
+                 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
                  ga('create', 'UA-51145039-1', 'lovesoltan.com');
                  ga('send', 'pageview');")
